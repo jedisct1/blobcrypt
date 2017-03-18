37c2bd9a53d98a5dcbe0dd28d3f9a66bc9a0eae8
Frank Denis <github@pureftpd.org>
2017-03-18 22:24:27 +0100

    Use argon2i instead of scrypt

--- example/key_derivation.c
+++ example/key_derivation.c
@@ -12,19 +12,14 @@
 int
 get_key_from_password(unsigned char *k, size_t k_bytes, int confirm)
 {
-#define SALT_PREFIX "Personalization for this example"
-#define SALT_PREFIX_LEN 32
-
     char          email[1024U];
     char          pwd2[1024U];
     char          pwd[1024U];
-    unsigned char salt[SALT_PREFIX_LEN + 1024U];
-    unsigned char h0[56];
+    unsigned char salt[crypto_pwhash_SALTBYTES];
     size_t        email_len;
     size_t        i;
     int           ret;
 
-    assert(strlen(SALT_PREFIX) == SALT_PREFIX_LEN);
     if (get_line(email, sizeof email, "Email: ") != 0) {
         return -1;
     }
@@ -55,15 +50,14 @@ get_key_from_password(unsigned char *k, size_t k_bytes, int confirm)
     }
     safe_write(2, "Deriving key from password... ",
                sizeof "Deriving key from password... " - 1U, -1);
-    crypto_generichash(h0, sizeof h0, (const unsigned char *) pwd,
-                       strlen(pwd), NULL, 0);
-    sodium_memzero(pwd, sizeof pwd);
-    memcpy(salt + SALT_PREFIX_LEN, email, email_len);
+    crypto_generichash(salt, sizeof salt, (const unsigned char *) email,
+                       email_len, NULL, 0);
     sodium_memzero(email, sizeof email);
-    ret = crypto_pwhash_scryptsalsa208sha256_ll(h0, sizeof h0, salt,
-                                                SALT_PREFIX_LEN + email_len,
-                                                1ULL << 18, 1U, 8U, k, k_bytes);
-    sodium_memzero(h0, sizeof h0);
+    ret = crypto_pwhash(k, k_bytes, pwd, strlen(pwd), salt,
+                        crypto_pwhash_OPSLIMIT_MODERATE,
+                        crypto_pwhash_MEMLIMIT_MODERATE,
+                        crypto_pwhash_ALG_DEFAULT);
+    sodium_memzero(pwd, sizeof pwd);
     sodium_memzero(salt, sizeof salt);
 
     safe_write(2, "done\n", sizeof "done\n" - 1U, -1);
